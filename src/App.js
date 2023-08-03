a73428c2fc78420fa3f86f89e7fe571822c890e3
André Bergan <andre.bergan@solv.no>
2023-08-03 21:56:26 +0200

    restructuring, cleanup + Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      hey
+      <Navbar />
       <header className="App-header">
         hey
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload. DAAAAAAYO
         </p>
